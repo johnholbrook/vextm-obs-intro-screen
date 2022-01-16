8d95bd5d70b4c9cae9d83b8e3fe17c45a7deab73
John Holbrook <3682581+johnholbrook@users.noreply.github.com>
2022-01-16 16:10:32 -0500

    Include subdirectories when copying site pages

--- copy-display-files.bat
+++ copy-display-files.bat
@@ -3,5 +3,5 @@ copy .\display.exe .\deploy\win32\build\VexTMIntro\display.exe
 xcopy /i .\icon\icon.ico .\deploy\win32\build\VexTMIntro\icon\
 xcopy /i .\node_modules\socket.io\client-dist\socket.io.min.js .\deploy\win32\build\VexTMIntro\node_modules\socket.io\client-dist\
 xcopy /i .\node_modules\bootstrap\dist\css\bootstrap.min.css .\deploy\win32\build\VexTMIntro\node_modules\bootstrap\dist\css\
-xcopy .\src\display\site\ .\deploy\win32\build\VexTMIntro\src\display\site\
+xcopy /s .\src\display\site\ .\deploy\win32\build\VexTMIntro\src\display\site\
 rename .\deploy\win32\build\VexTMIntro\qode.exe "VEX TM Intro Screen.exe"
